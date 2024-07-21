e77610a91df7115434ef3fbbfef9b65ca3db8822
FIREBOT <98863326+BOTMILLION@users.noreply.github.com>
2024-07-21 18:50:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 // Importando Firebase
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
-// Configuração do Firebase (Certifique-se de que o Firebase está inicializado em outro arquivo ou inclua aqui)
-const firebaseConfig = {
-    apiKey: "AIzaSyCKw5ZcJBcTvf1onPtkzgvJqlRAsbUqauk",
-    authDomain: "robo-7937c.firebaseapp.com",
-    projectId: "robo-7937c",
-    storageBucket: "robo-7937c.appspot.com",
-    messagingSenderId: "444396924434",
-    appId: "1:444396924434:web:46b93323f9c22d90ac32cb"
-};
-
 // Inicializa o Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
-initializeApp(firebaseConfig);
-
 const auth = getAuth();
 
 document.getElementById('loginButton').addEventListener('click', async function() {
