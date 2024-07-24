ca8e06f34376d7dc5e745ec4dfd1e0c71a32f3a0
FIREBOT <98863326+BOTMILLION@users.noreply.github.com>
2024-07-24 04:11:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js';
-import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js';
+// Importar as funções necessárias do SDK do Firebase
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDQZRg62a84f8KkvfSbH9IkKCsBH-66Tz0",
-    authDomain: "projeto-notificacao-968d4.firebaseapp.com",
-    projectId: "projeto-notificacao-968d4",
-    storageBucket: "projeto-notificacao-968d4.appspot.com",
-    messagingSenderId: "236002612799",
-    appId: "1:236002612799:web:54719603091421b94aca8a",
-    measurementId: "G-KY69HG6ZNZ"
+  apiKey: "AIzaSyDQZRg62a84f8KkvfSbH9IkKCsBH-66Tz0",
+  authDomain: "projeto-notificacao-968d4.firebaseapp.com",
+  projectId: "projeto-notificacao-968d4",
+  storageBucket: "projeto-notificacao-968d4.appspot.com",
+  messagingSenderId: "236002612799",
+  appId: "1:236002612799:web:54719603091421b94aca8a",
+  measurementId: "G-KY69HG6ZNZ"
 };
 
-// Inicializa o Firebase
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -25,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerLink = document.getElementById('registerLink');
     const loginButton = document.getElementById('loginButton');
     const registerButton = document.getElementById('registerButton');
-    const resetPasswordButton = document.getElementById('resetPasswordButton'); // Adicionado botão de redefinição de senha
     const paymentButton = document.getElementById('paymentButton');
     const redirectPopup = document.getElementById('redirectPopup');
     const redirectTimer = document.getElementById('redirectTimer');
@@ -51,49 +51,27 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('loginPassword').value;
         if (password.length < 6) {
             document.getElementById('loginError').style.display = 'block';
-            document.getElementById('loginError').textContent = 'A senha deve ter pelo menos 6 caracteres.';
         } else {
             try {
-                const userCredential = await signInWithEmailAndPassword(auth, email, password);
-                const user = userCredential.user;
-                const userDoc = doc(db, 'users', user.uid);
-                const userSnapshot = await getDoc(userDoc);
-
-                if (userSnapshot.exists()) {
-                    const userData = userSnapshot.data();
-                    const trialStartDate = new Date(userData.trialStartDate);
-                    const now = new Date();
-                    const trialPeriodDays = 3;
-                    const trialEndDate = new Date(trialStartDate);
-                    trialEndDate.setDate(trialEndDate.getDate() + trialPeriodDays);
-
-                    if (now <= trialEndDate) {
-                        // User is within the trial period
-                        loginForm.style.display = 'none';
-                        // Exibir o popup e iniciar o cronômetro
-                        redirectPopup.style.display = 'block';
-                        let countdown = 5;
-                        const countdownInterval = setInterval(() => {
-                            countdown -= 1;
-                            redirectTimer.textContent = countdown;
-                            if (countdown <= 0) {
-                                clearInterval(countdownInterval);
-                                window.location.href = 'https://botmillion.github.io/telm/'; // Alterar o link conforme necessário
-                            }
-                        }, 1000);
-
-                        // Redirecionar após o clique no botão do popup
-                        redirectNowButton.addEventListener('click', () => {
-                            window.location.href = 'https://botmillion.github.io/telm/'; // Alterar o link conforme necessário
-                        });
-                    } else {
-                        // Trial period has expired
-                        alert('Seu período de teste expirou. Por favor, faça o pagamento para continuar.');
-                        window.location.href = 'payment.html'; // Redirecionar para a página de pagamento
+                await signInWithEmailAndPassword(auth, email, password);
+                document.getElementById('loginError').style.display = 'none';
+                loginForm.style.display = 'none';
+                // Exibir o popup e iniciar o cronômetro
+                redirectPopup.style.display = 'block';
+                let countdown = 5;
+                const countdownInterval = setInterval(() => {
+                    countdown -= 1;
+                    redirectTimer.textContent = countdown;
+                    if (countdown <= 0) {
+                        clearInterval(countdownInterval);
+                        window.location.href = 'https://botmillion.github.io/telm/'; // Alterar o link conforme necessário
                     }
-                } else {
-                    console.error('No user data found');
-                }
+                }, 1000);
+
+                // Redirecionar após o clique no botão do popup
+                redirectNowButton.addEventListener('click', () => {
+                    window.location.href = 'https://botmillion.github.io/telm/'; // Alterar o link conforme necessário
+                });
             } catch (error) {
                 document.getElementById('loginError').style.display = 'block';
                 document.getElementById('loginError').textContent = error.message;
@@ -108,22 +86,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = document.getElementById('registerPassword').value;
         if (name === '' || email === '' || password === '' || password.length < 6) {
             document.getElementById('registerError').style.display = 'block';
-            document.getElementById('registerError').textContent = 'Por favor, preencha todos os campos corretamente.';
         } else {
             try {
-                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-                const user = userCredential.user;
-                await setDoc(doc(db, 'users', user.uid), {
+                await createUserWithEmailAndPassword(auth, email, password);
+                await addDoc(collection(db, 'users'), {
                     name: name,
-                    email: email,
-                    trialStartDate: new Date().toISOString() // Save the current date and time as trial start date
+                    email: email
                 });
                 document.getElementById('registerError').style.display = 'none';
                 registerForm.style.display = 'none';
-                // Enviar e-mail de verificação
-                await sendEmailVerification(user);
-                document.getElementById('registrationMessage').style.display = 'block';
-                document.getElementById('registrationMessage').textContent = 'Por favor, verifique seu e-mail.';
+                setTimeout(() => {
+                    alert('Cadastro realizado com sucesso!');
+                    loginForm.style.display = 'flex';
+                }, 500);
             } catch (error) {
                 document.getElementById('registerError').style.display = 'block';
                 document.getElementById('registerError').textContent = error.message;
@@ -131,24 +106,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Adicionado manipulador de clique para o botão de redefinição de senha
-    resetPasswordButton.addEventListener('click', async () => {
-        const email = document.getElementById('resetEmail').value;
-        try {
-            await sendPasswordResetEmail(auth, email);
-            document.getElementById('resetMessage').style.display = 'block';
-            document.getElementById('resetMessage').textContent = 'Email de redefinição de senha enviado.';
-        } catch (error) {
-            document.getElementById('resetError').style.display = 'block';
-            document.getElementById('resetError').textContent = error.message;
-        }
-    });
-
-    // Exibir o botão de pagamento se a página de pagamento estiver acessível
-    if (window.location.href.includes('payment.html')) {
-        paymentButton.style.display = 'block';
-        paymentButton.addEventListener('click', () => {
-            window.location.href = 'https://yampi.com.br/'; // Alterar para o link de pagamento
-        });
-    }
+    // Mostrar o botão de pagamento assim que a página carrega
+    paymentButton.style.display = 'block';
+    // Reiniciar a animação do botão de pagamento
+    paymentButton.classList.remove('pulse-button');
+    void paymentButton.offsetWidth; // Forçar reflow
+    paymentButton.classList.add('pulse-button');
 });
